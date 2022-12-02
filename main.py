c88fb067dc29bb0d5c822fb8f65135797e7de8b5
Roman <rom.tut.off@icloud.com>
2022-12-02 15:55:44 +0300

    2rd commit

--- main.py
+++ main.py
@@ -9,8 +9,19 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def sum_digit(number):
+    sum = 0
+    while number > 0:
+        sum += number % 10
+        number = number // 10
+    return sum
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
