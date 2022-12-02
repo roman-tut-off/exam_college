bcac566bbae0cb1ff85c29ba2cba275d6d0633d3
Roman <rom.tut.off@icloud.com>
2022-12-02 16:47:29 +0300

    Edit main func

--- main.py
+++ main.py
@@ -3,13 +3,6 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    res = sum_digit(input())
-    print(f'Ответ: {res}')
-
 def sum_digit(number):
     try:
         number = int(number)
@@ -24,9 +17,11 @@ def sum_digit(number):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    res = input('Введите число: ')
+    res = sum_digit(res)
+    print(res)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
 
 
 
